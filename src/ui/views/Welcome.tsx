af438c412a51ae55a6ea47f077e6ca66a7334db6
radiyx <34967185+radiyx@users.noreply.github.com>
2025-02-08 16:51:51 +0300

    Update Welcome.tsx

--- src/ui/views/Welcome.tsx
+++ src/ui/views/Welcome.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'antd';
 import { Link, useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
